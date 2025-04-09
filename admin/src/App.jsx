8b653fde0be57b1829d38a52ffab579bbf8d772a
Sandhya A R <itissandhyak@gmail.com>
2025-04-10 01:38:25 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://fooddeliverywebsite-backend-ellx.onrender.com/"
 
   return (
     <div>
